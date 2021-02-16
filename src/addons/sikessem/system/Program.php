031100c8802f1cc73e03d1cc49888ee296c0590b
SKE-SIKessEm <sikessem@gmail.com>
2021-02-16 21:36:38 +0100

    Add root directory validator from "src/addons/sikessem/system/Program.php"

--- src/addons/sikessem/system/Program.php
+++ src/addons/sikessem/system/Program.php
@@ -2,8 +2,21 @@
 
 abstract class Program
 {
+    public const INVALID_ROOT_VALUES = ['.', '..'];
+
+    public const INVALID_ROOT_ERROR = 0x01;
+
     public function __construct(string $root)
     {
+        if(empty($root))
+            throw new \InvalidArgumentException('Empty directory given', self::INVALID_ROOT_ERROR);
+            
+        if(in_array($root, self::INVALID_ROOT_VALUES))
+            throw new \InvalidArgumentException('Cannot use "' . $root . '" as directory', self::INVALID_ROOT_ERROR);
+        
+        if(!is_dir($root))
+            throw new \InvalidArgumentException('No such directory "' . $root . '"', self::INVALID_ROOT_ERROR);
+            
         $this->root = $root;
     }
 
