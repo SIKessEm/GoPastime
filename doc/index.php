7c76e0458f08b8473bb6342c47e368456cd797d6
SKE-SIKessEm <sikessem@gmail.com>
2021-03-01 18:27:36 +0100

    Use source class

--- doc/index.php
+++ doc/index.php
@@ -1,16 +1,33 @@
 <?php
+use Sikessem\{
+    Res,
+    Src,
+    Tpl
+};
 
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
-$sys = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'sys.php';
-
+$sys = require_once $root . 'sys.php';
 
 switch ($url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
 {
     case '/':
-        die('Home page');
+        $module = 'Home';
+        $action = 'index';
         break;
     
     default:
-        die('Error 404');
+        $module = 'ErrorDocument';
+        $action = 'error404';
         break;
 }
+
+$src = new Src($module . '.php', $root . 'src');
+$src->load();
+
+$module_class = 'App\\' . $module;
+$module_object = new $module_class;
+
+$module_object->$action();
+
+$tpl = new Tpl($root . 'tpl', 'jdhjj');
