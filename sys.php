80ea17b74577099fd6ed00640b1ade1161373986
SKE-SIKessEm <sikessem@gmail.com>
2021-03-01 14:57:13 +0100

    Update autoload path

--- sys.php
+++ sys.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once __DIR__ . '/lib/vendor/autoload.php';
+require_once __DIR__ . '/lib/autoload.php';
 
 return (array) parse_ini_file(__DIR__ . '/sys.ini', false, INI_SCANNER_TYPED);
